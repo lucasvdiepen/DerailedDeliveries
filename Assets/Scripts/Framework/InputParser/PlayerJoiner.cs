652517eb013053791710d0b8ad2289cd17ea203b
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2024-03-20 18:01:53 +0100

    Added player spawner test scene

---
+++ Assets/Scripts/Framework/InputParser/PlayerJoiner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerJoiner : MonoBehaviour
+{
+    private PlayerInputManager _playerInputManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
