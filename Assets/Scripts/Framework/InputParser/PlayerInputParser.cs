50230c0073942dfb2974f7f33317289285624340
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2024-03-20 10:55:20 +0100

    Added player input parser script

---
+++ Assets/Scripts/Framework/InputParser/PlayerInputParser.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DerailedDeliveries.Framework.InputParser
+{
+    public class PlayerInputParser : MonoBehaviour
+    {
+
+    }
+}
