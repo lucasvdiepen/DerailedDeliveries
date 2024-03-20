39b5cfabba08cbb21923cee00997ba202c5dcb70
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2024-03-20 18:10:53 +0100

    Added player prefab and code cleanup

--- Assets/Scripts/Framework/InputParser/PlayerInputParser.cs
+++ Assets/Scripts/Framework/InputParser/PlayerInputParser.cs
@@ -4,10 +4,21 @@ using UnityEngine.InputSystem;
 
 namespace DerailedDeliveries.Framework.InputParser
 {
+    /// <summary>
+    /// A class responsible for parsing player input and invoking events based on the input.
+    /// </summary>
     [RequireComponent(typeof(PlayerInput))]
     public class PlayerInputParser : MonoBehaviour
     {
+        /// <summary>
+        /// An event invoked when the player pressed the interact button.
+        /// </summary>
         public Action OnInteract;
+
+        /// <summary>
+        /// An event invoked when the player gives input to move.
+        /// Note that this event is only called when the input changes and not every frame.
+        /// </summary>
         public Action<Vector2> OnMove;
 
         private PlayerInput _playerInput;
