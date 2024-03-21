7223d7bda92a38110286b966642b56cf7792f2f1
Joost-Robat <30769@ma-web.nl>
2024-03-21 10:18:19 +0100

    Added require component to class, summary to class, player speed variable for tweaking speed and update function for setting the velocity of the player.

--- Assets/Scripts/Framework/Gameplay/Player/PlayerMovement.cs
+++ Assets/Scripts/Framework/Gameplay/Player/PlayerMovement.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 namespace DerailedDeliveries.Framework.Gameplay.Player
 {
-
+    /// <summary>
+    /// A class responsible for handling the player's movement.
+    /// </summary>
+    [RequireComponent(typeof(Rigidbody))]
     public class PlayerMovement : MonoBehaviour
     {
         [SerializeField]
@@ -12,17 +15,31 @@ namespace DerailedDeliveries.Framework.Gameplay.Player
 
         [SerializeField]
         private Vector2 _playerVelocity;
+
+        [SerializeField]
+        private float _playerSpeed = 2;
+
         private void Awake()
         {
             if(_playerRigidBody == null)
                 _playerRigidBody = gameObject.GetComponent<Rigidbody>();
         }
 
-        void Start()
+        private void OnEnable()
         {
-            SetMovementVector(new Vector2(10, 10));
+            
         }
 
         private void SetMovementVector(Vector2 playerInput) => _playerVelocity = playerInput;
+
+        private void FixedUpdate()
+        {
+            _playerRigidBody.velocity = new Vector3
+            (
+                _playerVelocity.x * _playerSpeed,
+                0, 
+                _playerVelocity.y * _playerSpeed
+            );
+        }
     }
 }
