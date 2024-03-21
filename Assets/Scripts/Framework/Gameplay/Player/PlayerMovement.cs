4ab9381c499739d54a4b9ffd3a3f0c02b8ade390
Joost-Robat <30769@ma-web.nl>
2024-03-21 11:40:23 +0100

    Changed SimulationMode on physics, made use of TimeManager OnTick instead of FixedUpdate, made changes to Player prefab.

--- Assets/Scripts/Framework/Gameplay/Player/PlayerMovement.cs
+++ Assets/Scripts/Framework/Gameplay/Player/PlayerMovement.cs
@@ -1,17 +1,24 @@
+using FishNet;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+using DerailedDeliveries.Framework.InputParser;
+using FishNet.Object;
 
 namespace DerailedDeliveries.Framework.Gameplay.Player
 {
     /// <summary>
     /// A class responsible for handling the player's movement.
     /// </summary>
-    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(Rigidbody), typeof(NetworkObject))]
     public class PlayerMovement : MonoBehaviour
     {
         [SerializeField]
         private Rigidbody _playerRigidBody;
+
+        [SerializeField]
+        private PlayerInputParser _playerInputParser;
 
         [SerializeField]
         private Vector2 _playerVelocity;
@@ -23,21 +30,34 @@ namespace DerailedDeliveries.Framework.Gameplay.Player
         {
             if(_playerRigidBody == null)
                 _playerRigidBody = gameObject.GetComponent<Rigidbody>();
+
+            if(_playerInputParser == null)
+                _playerInputParser = gameObject.GetComponent<PlayerInputParser>();
         }
 
         private void OnEnable()
         {
-            
+            _playerInputParser.OnMove += SetMovementVector;
+            InstanceFinder.TimeManager.OnTick += UpdateVelocity;
+        }
+
+        private void OnDisable()
+        {
+            _playerInputParser.OnMove -= SetMovementVector;
+            InstanceFinder.TimeManager.OnTick -= UpdateVelocity;
         }
 
         private void SetMovementVector(Vector2 playerInput) => _playerVelocity = playerInput;
 
-        private void FixedUpdate()
+        private void UpdateVelocity()
         {
+            if (_playerVelocity == Vector2.zero)
+                return;
+
             _playerRigidBody.velocity = new Vector3
             (
                 _playerVelocity.x * _playerSpeed,
-                0, 
+                _playerRigidBody.velocity.y, 
                 _playerVelocity.y * _playerSpeed
             );
         }
