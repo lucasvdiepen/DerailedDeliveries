5f6f401b6df6d62c034320055c449b94ea12a9be
PiterGrootHOME <piter.groot1104@gmail.com>
2024-03-21 12:57:54 +0100

    Implemented engine state

--- Assets/Scripts/Editor/TrainControllerEditor.cs
+++ Assets/Scripts/Editor/TrainControllerEditor.cs
@@ -6,13 +6,26 @@ using UnityEngine;
 [CustomEditor(typeof(TrainController))]
 public class TrainControllerEditor : Editor
 {
+    /// <summary>
+    /// Force editor to keep refreshing.
+    /// </summary>
+    /// <returns>True</returns>
+    public override bool RequiresConstantRepaint() => true;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
     public override void OnInspectorGUI()
     {
         DrawDefaultInspector();
         TrainController trainController = (TrainController)target;
 
         EditorGUI.BeginDisabledGroup(true);
-        EditorGUILayout.EnumPopup("Current Train Engine State: ", trainController.EngineState);
+
+        EditorGUILayout.EnumPopup("Current train engine state: ", trainController.EngineState);
+        EditorGUILayout.FloatField("Current train speed: ", trainController.CurrentVelocity * 100);
+        EditorGUILayout.FloatField("Current distance along spline: ", trainController.DistanceAlongSpline);
+
         EditorGUI.EndDisabledGroup();
     }
 }
--- Assets/Scripts/Framework/TrainController/TrainController.cs
+++ Assets/Scripts/Framework/TrainController/TrainController.cs
@@ -1,5 +1,6 @@
 using UnityEngine.Splines;
 using UnityEngine;
+using DG.Tweening;
 
 namespace DerailedDeliveries.Framework.TrainController
 {
@@ -10,16 +11,27 @@ namespace DerailedDeliveries.Framework.TrainController
     {
         [SerializeField]
         private SplineContainer _spline;
-
-        [SerializeField]
-        private float _speed = 1.0f;
-
-        [SerializeField]
-        private float _heightOffset = 1.0f;
-
+        
         [SerializeField, Range(0, 1)]
         private float _trainFrontStartTime = 0f;
 
+        [Header("Train Config")]
+        [SerializeField]
+        private float _maxSpeed = 7.5f;
+
+        [SerializeField]
+        private float _heightOffset = 1.0f;
+        
+        [SerializeField]
+        private float _accelerationDuration = 10f;
+
+        [SerializeField]
+        private Ease _accelerationEase = Ease.Linear;
+
+        [Header("Wagons Config")]
+        [SerializeField]
+        private Transform _frontWagon = null;
+        
         [SerializeField]
         private float _wagonFollowDistance = 0f;
 
@@ -27,26 +39,39 @@ namespace DerailedDeliveries.Framework.TrainController
         private float _wagonSpacing = 0f;
 
         [SerializeField]
-        private Transform _frontWagon = null;
-
-        [SerializeField]
         private Transform[] _wagons = null;
 
         /// <summary>
         /// Train velocity speed.
         /// </summary>
-        public float CurrentVelocity => _speed / 100f;
+        public float CurrentVelocity => _currentSpeed / 100f;
 
         /// <summary>
         /// Current train engine state.
         /// </summary>
         public TrainEngineState EngineState { get; set; }
 
-        private float _distanceAlongSpline = 0.0f;
+        /// <summary>
+        /// Distance value along spline lenght (same as time).
+        /// </summary>
+        public float DistanceAlongSpline { get; private set; } = 0.0f;
+        
+        private float _currentSpeed = 0f;
 
         private void Start()
         {
-            _distanceAlongSpline = _trainFrontStartTime;
+            DistanceAlongSpline = _trainFrontStartTime;
+            EngineState = TrainEngineState.ON_STANDBY;
+
+            var accelerationTween = DOTween.To(() 
+                => _currentSpeed, x => _currentSpeed = x, _maxSpeed, _accelerationDuration);
+
+            accelerationTween.OnStart(() => EngineState = TrainEngineState.ACCELERATING);
+            accelerationTween.OnComplete(() => EngineState = TrainEngineState.FULL_POWER);
+
+            DOTween.Sequence()
+                .AppendInterval(2f)
+                .Append(accelerationTween.SetEase(_accelerationEase));
         }
 
         private void OnValidate()
@@ -64,11 +89,11 @@ namespace DerailedDeliveries.Framework.TrainController
         /// <param name="offset"></param>
         public void UpdateWagonPosition(Transform trainBody, float offset = 0)
         {
-            Vector3 nextPosition = _spline.EvaluatePosition(_distanceAlongSpline + (offset / 10f));
+            Vector3 nextPosition = _spline.EvaluatePosition(DistanceAlongSpline + (offset / 10f));
             nextPosition.y += _heightOffset;
             trainBody.position = nextPosition;
 
-            Vector3 nextDirection = _spline.EvaluateTangent(_distanceAlongSpline + (offset / 10f));
+            Vector3 nextDirection = _spline.EvaluateTangent(DistanceAlongSpline + (offset / 10f));
             trainBody.rotation = Quaternion.LookRotation(-nextDirection, Vector3.up);
         }
 
@@ -85,14 +110,14 @@ namespace DerailedDeliveries.Framework.TrainController
                 UpdateWagonPosition(_wagons[i - 1], offset);
             }
 
-            _distanceAlongSpline += CurrentVelocity * Time.deltaTime;
-            if (_distanceAlongSpline > 1.0f)
-                _distanceAlongSpline = 0.0f;
+            DistanceAlongSpline += CurrentVelocity * Time.deltaTime;
+            if (DistanceAlongSpline > 1.0f)
+                DistanceAlongSpline = 0.0f;
         }
 
         private void DebugSnapToSpline()
         {
-            _distanceAlongSpline = _trainFrontStartTime;
+            DistanceAlongSpline = _trainFrontStartTime;
             UpdateWagonPosition(_frontWagon);
 
             int wagons = _wagons.Length + 1;
