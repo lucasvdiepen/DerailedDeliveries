704b354e83478fbd5a4dfa98eaeafd2cbfb1ef32
PiterGrootHOME <piter.groot1104@gmail.com>
2024-03-21 10:41:21 +0100

    Added wagon support for train controller

--- Assets/Scripts/Framework/TrainController/TrainController.cs
+++ Assets/Scripts/Framework/TrainController/TrainController.cs
@@ -17,40 +17,78 @@ namespace DerailedDeliveries.Framework.TrainController
         [SerializeField]
         private float _heightOffset = 1.0f;
 
+        [SerializeField, Range(0, 1)]
+        private float _trainFrontStartTime = 0f;
+
         [SerializeField]
-        private float _startDistance = 0f;
+        private float _wagonFollowDistance = 0f;
+
+        [SerializeField]
+        private float _wagonSpacing = 0f;
+
+        [SerializeField]
+        private Transform _frontWagon = null;
+
+        [SerializeField]
+        private Transform[] _wagons = null;
+
+        public float CurrentVelocity => _speed / 100f;
 
         private float _distanceAlongSpline = 0.0f;
 
         private void Start()
         {
-            _distanceAlongSpline = _startDistance;
+            _distanceAlongSpline = _trainFrontStartTime;
         }
 
-        [ContextMenu("Snap Train To Spline")]
+        private void OnValidate()
+        {
+            if (!gameObject.activeSelf || Application.isPlaying)
+                return;
+
+            DebugSnapToSpline();
+        }
+
         public void DebugSnapToSpline()
         {
-            _distanceAlongSpline = _startDistance;
+            _distanceAlongSpline = _trainFrontStartTime;
+            UpdateWagonPosition(_frontWagon);
 
-            Vector3 nextPosition = _spline.EvaluatePosition(_distanceAlongSpline);
+            int wagons = _wagons.Length + 1;
+            for (int i = 1; i < wagons; i++)
+            {
+                float adjustedFollowDistance = _wagonFollowDistance / 10f;
+                float offset = i == 1 ? adjustedFollowDistance : adjustedFollowDistance + (_wagonSpacing * i);
+                UpdateWagonPosition(_wagons[i - 1], offset);
+            }
+        }
+
+        /// <summary>
+        /// Sets a wagon transform to the correct spline position.
+        /// </summary>
+        /// <param name="trainBody"></param>
+        /// <param name="offset"></param>
+        public void UpdateWagonPosition(Transform trainBody, float offset = 0)
+        {
+            Vector3 nextPosition = _spline.EvaluatePosition(_distanceAlongSpline + (offset / 10f));
             nextPosition.y += _heightOffset;
-            transform.position = nextPosition;
+            trainBody.position = nextPosition;
 
-            Vector3 nextDirection = _spline.EvaluateTangent(_distanceAlongSpline);
-            transform.rotation = Quaternion.LookRotation(-nextDirection, Vector3.up);
+            Vector3 nextDirection = _spline.EvaluateTangent(_distanceAlongSpline + (offset / 10f));
+            trainBody.rotation = Quaternion.LookRotation(-nextDirection, Vector3.up);
         }
 
         private void Update()
         {
-            Vector3 nextPosition = _spline.EvaluatePosition(_distanceAlongSpline);
+            UpdateWagonPosition(_frontWagon);
 
-            nextPosition.y += _heightOffset;
-            transform.position = nextPosition;
+            int wagons = _wagons.Length + 1;
+            for (int i = 1; i < wagons; i++)
+            {
+                UpdateWagonPosition(_wagons[i - 1], _wagonSpacing * i);
+            }
 
-            Vector3 nextDirection = _spline.EvaluateTangent(_distanceAlongSpline);
-            transform.rotation = Quaternion.LookRotation(-nextDirection, Vector3.up);
-
-            _distanceAlongSpline += _speed * Time.deltaTime;
+            _distanceAlongSpline += CurrentVelocity * Time.deltaTime;
             if (_distanceAlongSpline > 1.0f)
                 _distanceAlongSpline = 0.0f;
         }
