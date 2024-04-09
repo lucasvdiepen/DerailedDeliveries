3e488675dcf715ec55c02b31841805507e8bb3f0
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2024-04-09 10:46:44 +0200

    Fixed the popup hover issue when moving

--- Assets/Scripts/Framework/PopupManegement/Popup.cs
+++ Assets/Scripts/Framework/PopupManegement/Popup.cs
@@ -23,13 +23,15 @@ namespace DerailedDeliveries.Framework.PopupManagement
         [SerializeField]
         private float _hoverDuration = 1.5f;
 
-        private TweenerCore<Vector3, Vector3, VectorOptions> _hoverAnimation;
+        private TweenerCore<float, float, FloatOptions> _hoverAnimation;
         private Coroutine _popupCoroutine;
-        private Vector3 _initialPosition;
+        private float _initialYPosition;
+        private float _endYPosition;
 
         private protected virtual void Awake()
         {
-            _initialPosition = _popupCanvasGroup.transform.position;
+            _initialYPosition = _popupCanvasGroup.transform.localPosition.y;
+            _endYPosition = _initialYPosition + _hoverHeight;
 
             _popupCanvasGroup.alpha = 0;
             _popupCanvasGroup.gameObject.SetActive(false);
@@ -75,11 +77,23 @@ namespace DerailedDeliveries.Framework.PopupManagement
 
             if(_hoverAnimation == null || !_hoverAnimation.active)
             {
-                _hoverAnimation = _popupCanvasGroup.transform.DOMoveY
+                _hoverAnimation = DOTween.To
                 (
-                    _popupCanvasGroup.transform.position.y + _hoverHeight,
+                    () => _initialYPosition, 
+                    y =>
+                    {
+                        _popupCanvasGroup.transform.localPosition = new Vector3
+                        (
+                            _popupCanvasGroup.transform.localPosition.x,
+                            y,
+                            _popupCanvasGroup.transform.localPosition.z
+                        );
+                    },
+                    _endYPosition,
                     _hoverDuration
-                ).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
+                )
+                .SetEase(Ease.InOutSine)
+                .SetLoops(-1, LoopType.Yoyo);
             }
 
             yield return _popupCanvasGroup.DOFade(1, _fadeDuration).SetEase(Ease.OutCubic).WaitForCompletion();
@@ -88,10 +102,6 @@ namespace DerailedDeliveries.Framework.PopupManagement
         private protected virtual IEnumerator ClosePopup()
         {
             yield return _popupCanvasGroup.DOFade(0, _fadeDuration).SetEase(Ease.OutCubic).WaitForCompletion();
-
-            _hoverAnimation.Kill();
-
-            _popupCanvasGroup.transform.position = _initialPosition;
 
             _popupCanvasGroup.gameObject.SetActive(false);
         }
