ae23fdb7f1dbf51e4c8cb49549293e13dc79a80c
PiterGrootHOME <piter.groot1104@gmail.com>
2024-04-11 10:15:23 +0200

    Fix build errors

--- Assets/TrainStationController.cs
+++ Assets/TrainStationController.cs
@@ -9,7 +9,7 @@ namespace DerailedDeliveries.Framework.Train
     /// <summary>
     /// Class responsible for stopping train and opening doors by stations.
     /// </summary>
-    public class TrainStationController : AbstractSingleton<TrainStationController>
+    public class TrainStationController : NetworkAbstractSingleton<TrainStationController>
     {
         [SerializeField]
         private float _minRangeToNearestStation = 100;
@@ -19,10 +19,10 @@ namespace DerailedDeliveries.Framework.Train
         /// </summary>
         /// <returns>True if there is an available station nearby.</returns>
         [ServerRpc(RequireOwnership = false)]
-        public bool TryDockTrainAtNearestStation()
+        public void TryDockTrainAtNearestStation()
         {
             Vector3 possibleStationPosition = StationManager.Instance.GetNearestStationDirector(transform.position);
-            return Vector3.Distance(possibleStationPosition, transform.position) > _minRangeToNearestStation;
+            //return Vector3.Distance(possibleStationPosition, transform.position) > _minRangeToNearestStation;
         }
     }
 }
