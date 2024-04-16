c6ea6225f859eb22749dd37bf7f3996ef9dfb160
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2024-04-16 16:09:42 +0200

    Added a working object parent finder

--- Assets/Scripts/Framework/ParentingSystem/ObjectParentFinder.cs
+++ Assets/Scripts/Framework/ParentingSystem/ObjectParentFinder.cs
@@ -1,14 +1,48 @@
-using System.Collections;
+using FishNet.Object;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace DerailedDeliveries.Framework.ParentingSystem
 {
-    public class ObjectParentFinder : MonoBehaviour
+    public class ObjectParentFinder : NetworkBehaviour
     {
+        private List<ObjectParent> _collidingParents = new();
+
         private void OnCollisionEnter(Collision collision)
         {
+            if(collision.transform.TryGetComponent(out ObjectParent objectParent))
+            {
+                objectParent.SetParent(NetworkObject);
+                _collidingParents.Add(objectParent);
+                return;
+            }
 
+            ObjectParent parentObjectParent = collision.transform.GetComponentInParent<ObjectParent>();
+            if(parentObjectParent == null)
+                return;
+
+            parentObjectParent.SetParent(NetworkObject);
+            _collidingParents.Add(parentObjectParent);
+        }
+
+        private void OnCollisionExit(Collision collision)
+        {
+            if(collision.transform.TryGetComponent(out ObjectParent objectParent))
+            {
+                NetworkObject.UnsetParent();
+                _collidingParents.Remove(objectParent);
+                return;
+            }
+
+            ObjectParent parentObjectParent = collision.transform.GetComponentInParent<ObjectParent>();
+            if(parentObjectParent == null)
+                return;
+
+            NetworkObject.UnsetParent();
+            _collidingParents.Remove(parentObjectParent);
+
+            foreach(ObjectParent parent in _collidingParents)
+                parent.SetParent(NetworkObject);
         }
     }
 }
