c40b98c45d0cbb207125fc543344a61f67f9667c
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2024-04-17 14:49:08 +0200

    Made parenting functionality for grabbables

--- Assets/Scripts/Framework/ParentingSystem/ObjectParentFinder.cs
+++ Assets/Scripts/Framework/ParentingSystem/ObjectParentFinder.cs
@@ -2,6 +2,8 @@ using FishNet.Object;
 using System.Collections.Generic;
 using UnityEngine;
 
+using DerailedDeliveries.Framework.Utils.ObjectParenting;
+
 namespace DerailedDeliveries.Framework.ParentingSystem
 {
     /// <summary>
@@ -9,50 +11,26 @@ namespace DerailedDeliveries.Framework.ParentingSystem
     /// </summary>
     public class ObjectParentFinder : NetworkBehaviour
     {
-        private List<ObjectParent> _collidingParents = new();
+        private readonly List<ObjectParent> _collidingParents = new();
 
         private void OnCollisionEnter(Collision collision)
         {
-            if (SetOrUnsetParent(collision, true))
+            if(!ObjectParentUtils.TryGetObjectParent(collision.gameObject, out ObjectParent objectParent))
                 return;
 
-            SetOrUnsetInParents(collision, true);
+            SetOrUnsetParent(objectParent, true);
         }
 
         private void OnCollisionExit(Collision collision)
         {
-            if (SetOrUnsetParent(collision, false))
+            if (!ObjectParentUtils.TryGetObjectParent(collision.gameObject, out ObjectParent objectParent))
                 return;
 
-            if(!SetOrUnsetInParents(collision, false))
-                return;
+            SetOrUnsetParent(objectParent, false);
 
             // If the parent is unset, check if there are any other parents inside collidingParents.
             foreach(ObjectParent parent in _collidingParents)
                 parent.SetParent(NetworkObject);
-        }
-
-        private bool SetOrUnsetInParents(Collision collision, bool isEntering)
-        {
-            ObjectParent parentObjectParent = collision.transform.GetComponentInParent<ObjectParent>();
-            if(parentObjectParent == null)
-                return false;
-
-            SetOrUnsetParent(parentObjectParent, isEntering);
-
-            return true;
-        }
-
-        private bool SetOrUnsetParent(Collision collision, bool isEntering)
-        {
-            bool state = collision.transform.TryGetComponent(out ObjectParent objectParent);
-
-            if (!state)
-                return false;
-
-            SetOrUnsetParent(objectParent, isEntering);
-
-            return true;
         }
 
         private void SetOrUnsetParent(ObjectParent objectParent, bool isEntering)
---
+++ Assets/Scripts/Framework/Utils/ObjectParenting/ObjectParentUtils.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+using DerailedDeliveries.Framework.ParentingSystem;
+
+namespace DerailedDeliveries.Framework.Utils.ObjectParenting
+{
+    /// <summary>
+    /// An util class for object parenting.
+    /// </summary>
+    public static class ObjectParentUtils
+    {
+        /// <summary>
+        /// Tries to get the <see cref="ObjectParent"/> of the given <see cref="GameObject"/>. This will also look in parents.
+        /// </summary>
+        /// <param name="gameObject">The <see cref="GameObject"/> to get the <see cref="ObjectParent"/> from.</param>
+        /// <param name="objectParent">The object parent when found.</param>
+        /// <returns>Whether the <see cref="ObjectParent"/> was found.</returns>
+        public static bool TryGetObjectParent(GameObject gameObject, out ObjectParent objectParent)
+        {
+            if(gameObject.TryGetComponent(out objectParent))
+                return true;
+
+            objectParent = gameObject.GetComponentInParent<ObjectParent>();
+
+            return objectParent != null;
+        }
+    }
+}
