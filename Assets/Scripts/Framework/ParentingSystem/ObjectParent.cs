839fe14a01a74a22566379f150d3e04016b51a28
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2024-04-17 11:25:45 +0200

    Code cleanup

--- Assets/Scripts/Framework/ParentingSystem/ObjectParent.cs
+++ Assets/Scripts/Framework/ParentingSystem/ObjectParent.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 namespace DerailedDeliveries.Framework.ParentingSystem
 {
+    /// <summary>
+    /// A class responsible for setting the parent of an object.
+    /// </summary>
     [RequireComponent(typeof(EmptyNetworkBehaviour))]
     public class ObjectParent : MonoBehaviour
     {
@@ -10,6 +13,10 @@ namespace DerailedDeliveries.Framework.ParentingSystem
 
         private void Awake() => _networkBehaviour = GetComponent<NetworkBehaviour>();
 
+        /// <summary>
+        /// Sets the parent of the given object to this object.
+        /// </summary>
+        /// <param name="networkObject">The object to set the parent of.</param>
         public void SetParent(NetworkObject networkObject) => networkObject.SetParent(_networkBehaviour);
     }
 }
--- Assets/Scripts/Framework/ParentingSystem/ObjectParentFinder.cs
+++ Assets/Scripts/Framework/ParentingSystem/ObjectParentFinder.cs
@@ -4,45 +4,68 @@ using UnityEngine;
 
 namespace DerailedDeliveries.Framework.ParentingSystem
 {
+    /// <summary>
+    /// A class responsible for finding parents for this object.
+    /// </summary>
     public class ObjectParentFinder : NetworkBehaviour
     {
         private List<ObjectParent> _collidingParents = new();
 
         private void OnCollisionEnter(Collision collision)
         {
-            if(collision.transform.TryGetComponent(out ObjectParent objectParent))
+            if (SetOrUnsetParent(collision, true))
+                return;
+
+            SetOrUnsetInParents(collision, true);
+        }
+
+        private void OnCollisionExit(Collision collision)
+        {
+            if (SetOrUnsetParent(collision, false))
+                return;
+
+            if(!SetOrUnsetInParents(collision, false))
+                return;
+
+            // If the parent is unset, check if there are any other parents inside collidingParents.
+            foreach(ObjectParent parent in _collidingParents)
+                parent.SetParent(NetworkObject);
+        }
+
+        private bool SetOrUnsetInParents(Collision collision, bool isEntering)
+        {
+            ObjectParent parentObjectParent = collision.transform.GetComponentInParent<ObjectParent>();
+            if(parentObjectParent == null)
+                return false;
+
+            SetOrUnsetParent(parentObjectParent, isEntering);
+
+            return true;
+        }
+
+        private bool SetOrUnsetParent(Collision collision, bool isEntering)
+        {
+            bool state = collision.transform.TryGetComponent(out ObjectParent objectParent);
+
+            if (!state)
+                return false;
+
+            SetOrUnsetParent(objectParent, isEntering);
+
+            return true;
+        }
+
+        private void SetOrUnsetParent(ObjectParent objectParent, bool isEntering)
+        {
+            if (isEntering)
             {
                 objectParent.SetParent(NetworkObject);
                 _collidingParents.Add(objectParent);
                 return;
             }
 
-            ObjectParent parentObjectParent = collision.transform.GetComponentInParent<ObjectParent>();
-            if(parentObjectParent == null)
-                return;
-
-            parentObjectParent.SetParent(NetworkObject);
-            _collidingParents.Add(parentObjectParent);
-        }
-
-        private void OnCollisionExit(Collision collision)
-        {
-            if(collision.transform.TryGetComponent(out ObjectParent objectParent))
-            {
-                NetworkObject.UnsetParent();
-                _collidingParents.Remove(objectParent);
-                return;
-            }
-
-            ObjectParent parentObjectParent = collision.transform.GetComponentInParent<ObjectParent>();
-            if(parentObjectParent == null)
-                return;
-
             NetworkObject.UnsetParent();
-            _collidingParents.Remove(parentObjectParent);
-
-            foreach(ObjectParent parent in _collidingParents)
-                parent.SetParent(NetworkObject);
+            _collidingParents.Remove(objectParent);
         }
     }
 }
