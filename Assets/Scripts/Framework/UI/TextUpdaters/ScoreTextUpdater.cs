7f71084edd5407b761d598a8c1b2b88464039737
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2024-04-24 16:08:23 +0200

    Added current score to UI

---
+++ Assets/Scripts/Framework/UI/TextUpdaters/ScoreTextUpdater.cs
@@ -0,0 +1,20 @@
+using DerailedDeliveries.Framework.Gameplay;
+
+namespace DerailedDeliveries.Framework.UI.TextUpdaters
+{
+    /// <summary>
+    /// A <see cref="TextUpdater"/> class that updates the current score text."/>
+    /// </summary>
+    public class ScoreTextUpdater : TextUpdater
+    {
+        private void OnEnable()
+        {
+            LevelTracker.Instance.OnScoreChanged += UpdateText;
+            UpdateText(LevelTracker.Instance.CurrentScore);
+        }
+
+        private void OnDisable() => LevelTracker.Instance.OnScoreChanged -= UpdateText;
+
+        private void UpdateText(int score) => ReplaceTag(score.ToString());
+    }
+}
