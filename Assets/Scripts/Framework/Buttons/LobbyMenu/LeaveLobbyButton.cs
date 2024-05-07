5b7fe8eff02b59b18fd128c415149f65f0de6405
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2024-05-07 15:09:45 +0200

    Fixed leave button when not connected to the host

--- Assets/Scripts/Framework/Buttons/LobbyMenu/LeaveLobbyButton.cs
+++ Assets/Scripts/Framework/Buttons/LobbyMenu/LeaveLobbyButton.cs
@@ -1,11 +1,19 @@
 using FishNet;
 
+using DerailedDeliveries.Framework.StateMachine.States;
+
 namespace DerailedDeliveries.Framework.Buttons.LobbyMenu
 {
     public class LeaveLobbyButton : BasicButton
     {
         private protected override void OnButtonPressed()
         {
+            if(InstanceFinder.NetworkManager.IsOffline)
+            {
+                StateMachine.StateMachine.Instance.GoToState<MainMenuState>();
+                return;
+            }
+
             InstanceFinder.ServerManager.StopConnection(true);
             InstanceFinder.ClientManager.StopConnection();
         }
