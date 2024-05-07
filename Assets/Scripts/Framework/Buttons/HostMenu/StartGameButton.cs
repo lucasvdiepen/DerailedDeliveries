6d02a62d8ad9949548fabb1972a909aa55d495ab
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2024-05-07 13:32:44 +0200

    Added button disable when not enough players

--- Assets/Scripts/Framework/Buttons/HostMenu/StartGameButton.cs
+++ Assets/Scripts/Framework/Buttons/HostMenu/StartGameButton.cs
@@ -1,4 +1,5 @@
 using DerailedDeliveries.Framework.GameManagement;
+using DerailedDeliveries.Framework.PlayerManagement;
 
 namespace DerailedDeliveries.Framework.Buttons.HostMenu
 {
@@ -8,5 +9,25 @@ namespace DerailedDeliveries.Framework.Buttons.HostMenu
     public class StartGameButton : BasicButton
     {
         private protected override void OnButtonPressed() => GameManager.Instance.StartGame();
+
+        private protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            PlayerManager.Instance.OnPlayersUpdated += UpdateButtonInteractivity;
+            UpdateButtonInteractivity();
+        }
+
+        private protected override void OnDisable()
+        {
+            base.OnDisable();
+
+            if(PlayerManager.Instance == null)
+                return;
+
+            PlayerManager.Instance.OnPlayersUpdated -= UpdateButtonInteractivity;
+        }
+
+        private void UpdateButtonInteractivity() => Button.interactable = PlayerManager.Instance.PlayerCount > 0;
     }
 }
