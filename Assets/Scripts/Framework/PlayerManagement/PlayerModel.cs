ab1e811bd2f51e8022ade231a6a0038088d40a8c
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2024-05-08 15:48:53 +0200

    Added summaries

--- Assets/Scripts/Framework/PlayerManagement/PlayerModel.cs
+++ Assets/Scripts/Framework/PlayerManagement/PlayerModel.cs
@@ -2,8 +2,14 @@ using UnityEngine;
 
 namespace DerailedDeliveries.Framework.PlayerManagement
 {
+    /// <summary>
+    /// A class responsible for holding the model index of a player.
+    /// </summary>
     public class PlayerModel : MonoBehaviour
     {
+        /// <summary>
+        /// The index of the model.
+        /// </summary>
         public int ModelIndex { get; set; }
     }
 }
