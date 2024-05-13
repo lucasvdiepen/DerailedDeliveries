9f89c3eec5ccd23861f2dd77f38081efe75cbe87
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2024-05-13 10:06:54 +0200

    Added a camera rotator for the background of the menus

---
+++ Assets/Scripts/Framework/Camera/CameraRotator.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+using DerailedDeliveries.Framework.StateMachine;
+using DerailedDeliveries.Framework.StateMachine.States;
+
+namespace DerailedDeliveries.Framework.Camera
+{
+    /// <summary>
+    /// A class responsible for rotating the camera for the background of the menu screens.
+    /// </summary>
+    public class CameraRotator : MonoBehaviour
+    {
+        [SerializeField]
+        private float _speed = 5f;
+
+        private void OnEnable() => StateMachine.StateMachine.Instance.OnStateChanged += OnStateChanged;
+
+        private void OnDisable() => StateMachine.StateMachine.Instance.OnStateChanged -= OnStateChanged;
+
+        private void OnStateChanged(State state) => gameObject.SetActive(state is not GameState);
+
+        private void Update() => transform.Rotate(Vector3.up, _speed * Time.deltaTime);
+    }
+}
