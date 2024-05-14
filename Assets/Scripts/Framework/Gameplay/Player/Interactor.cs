7c967454c9764089fa6a94b33d09e80a8dd8a819
Joost-Robat <joost_wouter@icloud.com>
2024-05-14 14:42:45 +0200

    Changed grab button to F on keyboard to avoid confusion.

--- Assets/Scripts/Framework/Gameplay/Player/Interactor.cs
+++ Assets/Scripts/Framework/Gameplay/Player/Interactor.cs
@@ -56,9 +56,9 @@ namespace DerailedDeliveries.Framework.Gameplay.Player
             _collider = GetComponent<SphereCollider>();
         }
 
-        private void OnEnable() => _inputParser.OnUse += UseInteractable;
+        private void OnEnable() => _inputParser.OnGrab += UseInteractable;
 
-        private void OnDisable() => _inputParser.OnUse -= UseInteractable;
+        private void OnDisable() => _inputParser.OnGrab -= UseInteractable;
 
         private void UseInteractable()
         {
