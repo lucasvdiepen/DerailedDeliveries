6fab893973ea1eacaccd945933f7a9066e12af73
PiterGrootHOME <piter.groot1104@gmail.com>
2024-05-17 09:21:38 +0200

    Fix doors not opening in build

--- Assets/Scripts/Framework/Train/TrainDoorsAnimator.cs
+++ Assets/Scripts/Framework/Train/TrainDoorsAnimator.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 
+using DerailedDeliveries.Framework.StateMachine;
+using DerailedDeliveries.Framework.StateMachine.States;
+
 namespace DerailedDeliveries.Framework.Train
 {
     /// <summary>
@@ -15,8 +18,13 @@ namespace DerailedDeliveries.Framework.Train
 
         private void Awake() => _doorAnimator = GetComponent<Animator>();
 
-        private void OnEnable()  
+        private void OnEnable() => StateMachine.StateMachine.Instance.OnStateChanged += HandleStateChanged;
+
+        private void HandleStateChanged(State state)
         {
+            if (state is not GameState)
+                return;
+
             TrainStationController.Instance.OnParkStateChanged += HandleParkStateChanged;
             HandleParkStateChanged(true);
         }
@@ -25,6 +33,9 @@ namespace DerailedDeliveries.Framework.Train
         {
             if(TrainStationController.Instance != null) 
                 TrainStationController.Instance.OnParkStateChanged -= HandleParkStateChanged;
+
+            if(StateMachine.StateMachine.Instance != null)
+                StateMachine.StateMachine.Instance.OnStateChanged -= HandleStateChanged;
         }
 
         private void Start()
@@ -33,7 +44,7 @@ namespace DerailedDeliveries.Framework.Train
             _doorsCloseAnimationHash = Animator.StringToHash("DoorsClose");
         }
 
-        private void HandleParkStateChanged(bool newParkState) 
+        private void HandleParkStateChanged(bool newParkState)
             => _doorAnimator.SetTrigger(newParkState ? _doorsOpenAnimationHash : _doorsCloseAnimationHash);
     }
 }
